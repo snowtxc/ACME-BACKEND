2dfa8e9f3e47d900d0a92748ec096ef960a634ae
Manuel Diu <63983711+ManuelDiu@users.noreply.github.com>
2023-10-28 16:50:11 -0300

    fix program.cs implementation

---
+++ DataAccessLayer/Migrations/20231028194349_LookAndFeelImplementation.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class LookAndFeelImplementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NavBarId",
+                table: "LooksAndFeels",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorFondo",
+                table: "LooksAndFeels",
+                type: "longtext",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorPrincipal",
+                table: "LooksAndFeels",
+                type: "longtext",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorSecundario",
+                table: "LooksAndFeels",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorFondo",
+                table: "LooksAndFeels");
+
+            migrationBuilder.DropColumn(
+                name: "ColorPrincipal",
+                table: "LooksAndFeels");
+
+            migrationBuilder.DropColumn(
+                name: "ColorSecundario",
+                table: "LooksAndFeels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NavBarId",
+                table: "LooksAndFeels",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- DataAccessLayer/Program.cs
+++ DataAccessLayer/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using DataAccessLayer.Db;
+using DataAccessLayer.Models;
+using DataAccessLayer.IDALs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseMySQL("Server=localhost;Database=acme;User=root;Password=;"));
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddIdentity<Usuario, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+string secret = "JWTAuthenticationHIGHsecuredPasswordVVVp1OH7Xzyr";
+
+var key = Encoding.ASCII.GetBytes(secret); // Reemplaza con tu propia clave secreta
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // En producci�n, configura esto como true para usar HTTPS
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, // Reemplaza con tu emisor si es necesario
+        ValidateAudience = false // Reemplaza con tu audiencia si es necesario
+    };
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+
+var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+db.Database.Migrate();
+
+async Task CreateDefaultRoles(IServiceScope scopeContext)
+{
+    var roleManager = scopeContext.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new[] { "Admin", "Vendedor", "Usuario" };
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
+await CreateDefaultRoles(scope);
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(builder => builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
